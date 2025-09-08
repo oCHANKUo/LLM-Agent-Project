da609eb90d390cd2d2495c760ac742977399b948
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-09-08 13:37:04 +0530

    SQL Tool.py

--- tools/sql_tool.py
+++ tools/sql_tool.py
@@ -3,3 +3,15 @@ from langchain.chains import SQLDatabaseChain
 from langchain.llms import LlamaCpp
 import config
 
+llm = LlamaCpp(model_path=config.LLAMA_MODEL_PATH)
+
+db = SQLDatabase.from_uri(config.DB_CONNECTION_STRING)
+
+sql_chain = SQLDatabaseChain(
+    llm, db, verbose=True
+)
+
+# Tool Function
+def text_to_sql(query: str) -> str:
+    return sql_chain.run(query)
+
