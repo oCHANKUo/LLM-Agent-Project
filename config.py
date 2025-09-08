ec1bfdf283d1fe34a7808c1393830e53f8aa7b41
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-09-08 16:17:53 +0530

    model path changes

--- config.py
+++ config.py
@@ -4,4 +4,5 @@
 DB_CONNECTION_STRING = "mssql+pyodbc://admin:admin@server/database?driver=ODBC+Driver+17+for+SQL+Server"
 
 # LLaMA 3 Model Path
-LLAMA_MODEL_PATH = "C:\Users\chanu\.ollama\models\manifests\registry.ollama.ai\library\llama3.1"
+LLAMA_MODEL_PATH = "C:/Users/chanu/.ollama/models/manifests/registry.ollama.ai/library/llama3.1"
+
