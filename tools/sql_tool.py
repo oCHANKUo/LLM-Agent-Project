db18146b4da1ad2adf27bce0c1aff2a9f58d2ee8
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-09-08 16:02:50 +0530

    changes

--- tools/sql_tool.py
+++ tools/sql_tool.py
@@ -1,5 +1,5 @@
 from langchain_community.utilities import SQLDatabase
-from langchain.chains import SQLDatabaseChain
+from langchain.chains import create_sql_query_chain
 from langchain_community.llms import LlamaCpp
 import config
 
@@ -7,7 +7,7 @@ llm = LlamaCpp(model_path=config.LLAMA_MODEL_PATH)
 
 db = SQLDatabase.from_uri(config.DB_CONNECTION_STRING)
 
-sql_chain = SQLDatabaseChain(
+sql_chain = create_sql_query_chain(
     llm, db, verbose=True
 )
 
